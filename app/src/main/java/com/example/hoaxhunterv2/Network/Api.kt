ddbd3917595b922fb4265dbda86ae001685752eb
husain502 <husainduta@gmail.com>
2021-10-27 23:05:36 +0700

    add room

--- app/src/main/java/com/example/hoaxhunterv2/Network/Api.kt
+++ app/src/main/java/com/example/hoaxhunterv2/Network/Api.kt
@@ -3,9 +3,11 @@ package com.example.hoaxhunterv2.Network
 import com.example.hoaxhunterv2.model.HoaxResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import java.util.*
 
 interface Api {
     @GET("antihoax")
+    @Headers("Authorization:43d93c474a0e06142a9a4c52484e6910")
     suspend fun getHoax(): Call<ArrayList<HoaxResponse>>
 }
