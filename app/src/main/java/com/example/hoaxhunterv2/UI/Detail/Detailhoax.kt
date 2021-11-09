ca0105573834d94e08926a5ff6f56bb4925b722f
husain502 <husainduta@gmail.com>
2021-11-09 23:46:29 +0700

    fix mainactivity

---
+++ app/src/main/java/com/example/hoaxhunterv2/UI/Detail/Detailhoax.kt
@@ -0,0 +1,4 @@
+package com.example.hoaxhunterv2.UI.Detail
+
+class Detailhoax {
+}
--- app/src/main/java/com/example/hoaxhunterv2/UI/Main/Mainadapter.kt
+++ app/src/main/java/com/example/hoaxhunterv2/UI/Main/Mainadapter.kt
@@ -1,10 +1,15 @@
 package com.example.hoaxhunterv2.UI.Main
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.hoaxhunterv2.R
+import com.example.hoaxhunterv2.UI.Detail.Detailhoax
 import com.example.hoaxhunterv2.data.Hoax
 import com.example.hoaxhunterv2.databinding.NewsBinding
 
@@ -23,10 +28,42 @@ class Mainadapter: RecyclerView.Adapter<Mainadapter.MyViewHolder>(){
         fun bind (hoax: Hoax){
 
             binding.apply {
-                tanggal.text =
-
+                picture1.loadImage(hoax.picture1)
+                title.text = hoax.title
+                fact.text = hoax.fact
+                typehoaks.text = hoax.classification
             }
         }
     }
+    private  fun ImageView.loadImage(url:String?){
+        Glide.with(this.context)
+                .load(url)
+                .apply(RequestOptions().override(500,500))
+                .into(this)
+    }
 
- }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = NewsBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+       val pos = listhoax[position]
+        holder.bind(pos)
+        holder.itemView.setOnClickListener{
+            val intent = Intent(holder.itemView.context,Detailhoax::class.java).apply {
+                putExtra(Detailhoax.EXTRA_TITLE,pos.title)
+                putExtra(Detailhoax.EXTRA_ID,pos.id)
+                putExtra(Detailhoax.EXTRA_PICTURE,pos.picture1)
+                putExtra(Detailhoax.EXTRA_CLASSIFICATION,pos.classification)
+                putExtra(Detailhoax.EXTRA_FACT,pos.fact)
+            }
+            it.context.startActivity(intent)
+        }
+    }
+
+    override fun getItemCount(): Int = listhoax.size
+    }
+
+
+}
